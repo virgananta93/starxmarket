89c0ba0976da0dab6edd25f177993824f5554d2b
virgananta <virgafallerie2@gmail.com>
2020-09-23 14:18:02 +0800

    FIRST BATCH

---
+++ src/components/Navbar.vue
@@ -0,0 +1,71 @@
+<template>
+  <b-container fluid>
+    <b-navbar toggleable="lg" type="dark" class="fixed-top" sticky>
+      <b-navbar-toggle target="nav-collapse" class="ml-auto">
+        <template v-slot:default="{ expanded }">
+          <b-icon v-if="expanded" icon="chevron-compact-up" variant="dark"></b-icon>
+          <b-icon v-else icon="list" variant="dark"></b-icon>
+        </template>
+      </b-navbar-toggle>
+
+      <!-- Left aligned nav items -->
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Exchange</b-nav-item>
+          <b-nav-item to="/">Product</b-nav-item>
+          <b-nav-item to="/">Service</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/">Login</b-nav-item>
+          <b-nav-item class="register" pill variant="outline-warning" to="/">Register</b-nav-item>
+          <b-nav-item to="/">Support</b-nav-item>
+
+          <!-- Language items -->
+          <b-nav-item-dropdown right caret>
+            <template v-slot:button-content>
+              <img
+                src="https://ik.imagekit.io/virgananta/tr:w-30,h-30,lo-true/StarWork/small_asset/language_icon_i6fdx21ChR.png"
+              />
+              <span class="sr-only">Language</span>
+            </template>
+            <b-dropdown-item href="#">ENG</b-dropdown-item>
+            <b-dropdown-item href="#">ID</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style lang="scss" scoped>
+.nav-collapse .navbar-nav {
+    text-align: right;
+}
+
+.navbar-nav>li {
+    padding: 0 50px;
+
+    a {
+        color: #e2ac2f !important;
+    }
+}
+
+.navbar-toggler {
+    background-color: #ddc282 !important;
+}
+
+@media all and (max-width: 768px) {
+    .navbar-nav>li {
+        width: 50%;
+        margin: 0 0 0 auto;
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+   <Hero />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Hero from '@/components/Hero.vue'
+export default {
+  name: 'Home',
+    components: {
+    Hero,
+  }
+}
+</script>
